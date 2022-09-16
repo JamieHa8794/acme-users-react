a68c0ea002ce23a271a174a42d975bbb228daafd
JamieHa8794 <JamieHa8794@gmail.com>
2022-09-16 17:25:46 -0400

    refactor

--- server.js
+++ server.js
@@ -1,45 +1,14 @@
-const Sequelize = require('sequelize')
-const { STRING, TEXT } = Sequelize
+const {db, syncAndSeed, models: {User}} = require('./db/index.js')
 
-const faker = require('faker')
 
-const db =  new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/acme_users_react_db');
-
-const User = db.define('user', 
-    {
-        name: {
-            type: STRING
-        },
-        bio:{
-            type: TEXT
-        }
-    },
-    {
-        hooks:{
-            beforeCreate: function(user){
-                if(!user.bio){
-                    user.bio = `${user.name} ${faker.lorem.paragraphs(2)} ${user.name}`;
-                }
-            }
-        }
-    }
-)
-
-User.createWithName = (name) =>{
-    return(
-        User.create({name})
-    )
-}
-
-const syncAndSeed = async () =>{
+const init = async () =>{
     try{
-        await db.sync({force: true})
-        const [moe, larry, curly, ethyl] = await Promise.all(['Moe', 'Larry', 'Curly', 'Ethyl'].map(User.createWithName))
-        console.log('connected to db')
+        await syncAndSeed();
+        
     }
     catch(err){
         console.log(err)
     }
 }
 
-syncAndSeed();
+init();
