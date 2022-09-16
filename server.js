636bdbed902f6669aa3f1a289d8c4631c25201bc
JamieHa8794 <JamieHa8794@gmail.com>
2022-09-16 17:34:59 -0400

    set up express & /api/users

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
-const {db, syncAndSeed, models: {User}} = require('./db/index.js')
+const { syncAndSeed, models: { User } } = require('./db')
+
+const express = require('express')
+const app = express();
+
+app.get('/api/users', async (req, res, next)=>{
+    try{
+        const data = await User.findAll();
+        res.send(data)
+    }
+    catch(err){
+        next(err);
+    }
+})
 
 
 const init = async () =>{
     try{
         await syncAndSeed();
+        const port = process.env.URL || 3000;
+        app.listen(port, ()=>console.log(`listening on port ${port}`))
         
     }
     catch(err){
