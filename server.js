d0ff0dcfcb306075c4ce832b549a56b467ba9c45
JamieHa8794 <JamieHa8794@gmail.com>
2022-09-16 18:06:19 -0400

    set up routes, html, dist, styles

--- server.js
+++ server.js
@@ -3,9 +3,31 @@ const { syncAndSeed, models: { User } } = require('./db')
 const express = require('express')
 const app = express();
 
+const path = require('path')
+
+app.get('/', (req, res, next)=> res.sendFile(path.join(__dirname, 'index.html')));
+
+app.use('/dist', express.static(path.join(__dirname, 'dist')));
+app.use('/src', express.static(path.join(__dirname, 'src')))
+
+
 app.get('/api/users', async (req, res, next)=>{
     try{
-        const data = await User.findAll();
+        const data = await User.findAll({
+            attributes:{
+                exclude: ['bio']
+            }
+        });
+        res.send(data)
+    }
+    catch(err){
+        next(err);
+    }
+})
+
+app.get('/api/users/:id', async (req, res, next)=>{
+    try{
+        const data = await User.findByPk(req.params.id)
         res.send(data)
     }
     catch(err){
@@ -17,7 +39,7 @@ app.get('/api/users', async (req, res, next)=>{
 const init = async () =>{
     try{
         await syncAndSeed();
-        const port = process.env.URL || 3000;
+        const port = process.env.PORT || 3000;
         app.listen(port, ()=>console.log(`listening on port ${port}`))
         
     }
