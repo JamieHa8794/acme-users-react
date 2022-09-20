8e1b837bee8691ce3a7f2b63a1ed5125587370ca
JamieHa8794 <JamieHa8794@gmail.com>
2022-09-19 21:07:10 -0400

    get and display users as links

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import React, {Component} from 'react'
+import axios from 'axios'
 
 class App extends Component{
+    constructor(){
+        super();
+        this.state = {
+            users: []
+        }
+    }
+    async componentDidMount(){
+        const users = (await axios.get('/api/users')).data
+        this.setState({users})
+    }
     render(){
+        const { users } = this.state;
         return(
-            <h1>
-                Hello World
-            </h1>
+            <div>
+                <h1>
+                    Acme Users
+                </h1>
+                <ul>
+                    {
+                        users.map( (user,idx) =>{
+                            return(
+                                <li key={idx}>
+                                    <a href={`#${user.id}`}>
+                                    { user.name } 
+                                    </a>
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
+            </div>
         )
     }
 }
